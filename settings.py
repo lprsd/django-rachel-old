5039f499607a11f7223830737bf24e4f3629e539
Lakshman Prasad <lakshman@uswaretech.com>
2010-08-19 22:09:59 +0530

    Markup types

--- settings.py
+++ settings.py
@@ -81,4 +81,16 @@ INSTALLED_APPS = (
     'taggit',
 )
 
+import markdown
+from docutils.core import publish_parts
+
+def render_rest(markup):
+    parts = publish_parts(source=markup, writer_name="html4css1")
+    return parts["fragment"]
+
+MARKUP_FIELD_TYPES = (
+    ('markdown', markdown.markdown),
+    ('ReST', render_rest),
+)
+
 from localsettings import *
